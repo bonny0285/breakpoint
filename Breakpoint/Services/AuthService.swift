b2db302f1fca5c170bb20e20b170004ca835ba10
Massimiliano <massimiliano@MacBook-Pro-di-Massimiliano.local>
2019-05-14 20:23:41 +0100

    log and register func

---
+++ Breakpoint/Services/AuthService.swift
@@ -0,0 +1,38 @@
+//
+//  AuthService.swift
+//  Breakpoint
+//
+//  Created by Massimiliano on 14/05/2019.
+//  Copyright © 2019 Massimiliano Bonafede. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class AuthService{
+    static let instace = AuthService()
+    
+    func registerUser(withEmail email: String, andPassword password: String, userCreationComplete: @escaping (_ status: Bool, _ error: Error?) -> ()){
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            guard let user = user?.user else {
+                userCreationComplete(false, error)
+                return
+            }
+            let userData = ["provider": user.providerID, "email": user.email]
+            DataService.instance.createDBUser(uid: user.uid, userData: userData)
+            userCreationComplete(true, nil)
+        }
+    }
+    
+    func logUser(withEmail email: String, andPassword password: String, loginComplete: @escaping (_ status: Bool, _ error: Error?) -> ()){
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            guard let user = user?.user else {
+                loginComplete(false,error)
+                return
+            }
+            
+            loginComplete(true,nil)
+        }
+    }
+}
